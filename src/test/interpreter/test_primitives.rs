f7df4c8d6339ccc173abf06e245acfbd14a68c30
Alexander Smoktal <cosm.ua@gmail.com>
2018-09-13 16:58:24 +0300

    Interpreter test utils modification

--- src/test/interpreter/test_primitives.rs
+++ src/test/interpreter/test_primitives.rs
@@ -1,19 +1,21 @@
-use super::utils::interpret_epression;
+use ast::rules;
+
+use super::utils::interpret_rule;
 
 #[test]
 fn test_primitives() {
-    let (val, mut _env) = interpret_epression("nil");
+    let (val, mut _env) = interpret_rule("nil", rules::exp);
     assert_eq!(val, "Nil");
 
-    let (val, _env) = interpret_epression("true");
+    let (val, _env) = interpret_rule("true", rules::exp);
     assert_eq!(val, "Boolean(true)");
 
-    let (val, _env) = interpret_epression("false");
+    let (val, _env) = interpret_rule("false", rules::exp);
     assert_eq!(val, "Boolean(false)");
 
-    let (val, _env) = interpret_epression("42.4");
+    let (val, _env) = interpret_rule("42.4", rules::exp);
     assert_eq!(val, "Number(42.4)");
 
-    let (val, _env) = interpret_epression(r#""Hello world""#);
+    let (val, _env) = interpret_rule(r#""Hello world""#, rules::exp);
     assert_eq!(val, r#"String("Hello world")"#);
 }
--- src/test/interpreter/utils.rs
+++ src/test/interpreter/utils.rs
@@ -7,24 +7,16 @@ use interpreter::environment;
 
 pub fn interpret(source_code: &str) -> (types::Type, environment::Environment)
 {
-    let mut parser = parser::Parser::new(source_code.to_string());
-    let mut stack = stack::Stack::new();
-
-    rules::chunk(&mut parser, &mut stack);
-
-    assert!(parser.peek().is_none(), format!("Parser contains tokens after parsing: {:?}", parser));
-
-    let mut env = environment::Environment::new(None);
-
-    (stack.pop_single().eval(&mut env), env)
+    interpret_rule(source_code, rules::chunk)
 }
 
-pub fn interpret_epression(source_code: &str) -> (types::Type, environment::Environment)
+pub fn interpret_rule<F>(source_code: &str, func: F) -> (types::Type, environment::Environment) where
+    F: Fn(&mut parser::Parser, &mut stack::Stack) -> bool
 {
     let mut parser = parser::Parser::new(source_code.to_string());
     let mut stack = stack::Stack::new();
 
-    rules::exp(&mut parser, &mut stack);
+    func(&mut parser, &mut stack);
 
     assert!(parser.peek().is_none(), format!("Parser contains tokens after parsing: {:?}", parser));
 
