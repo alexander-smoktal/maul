61cbae4ff8553da41ff52c50895dd98794fe0d60
Alexander Smoktal <cosm.ua@gmail.com>
2018-10-24 01:25:43 +0300

    Minor cleanup

--- src/ast/expressions/operators.rs
+++ src/ast/expressions/operators.rs
@@ -47,20 +47,6 @@ impl Binop {
     }
 
     // https://en.wikipedia.org/wiki/Operator-precedence_parser
-    // parse_expression_1 (lhs, min_precedence)
-    //     lookahead := peek next token
-    //     while lookahead is a binary operator whose precedence is >= min_precedence
-    //         op := lookahead
-    //         advance to next token
-    //         rhs := parse_primary ()
-    //         lookahead := peek next token
-    //         while lookahead is a binary operator whose precedence is greater
-    //                  than op's, or a right-associative operator
-    //                  whose precedence is equal to op's
-    //             rhs := parse_expression_1 (rhs, lookahead's precedence)
-    //             lookahead := peek next token
-    //         lhs := the result of applying op with operands lhs and rhs
-    //     return lhs
     pub fn prec_rule(
         parser: &mut parser::Parser,
         stack: &mut stack::Stack,
